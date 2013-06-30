3e72e22320c89ca561f59099531b43e968cd6e6f
skandhas <skandhas@163.com>
2013-06-30 23:14:37 +0800

    update plotter/gl.h

--- include/plotter/gl.h
+++ include/plotter/gl.h
@@ -4,6 +4,11 @@
 #ifdef WIN32
 #include "gl/glew.h"
 #include "gl/wglew.h"
+#elif __APPLE__
+    #include "TargetConditionals.h"
+    #ifdef TARGET_OS_MAC
+        #include <OpenGL/gl.h>
+    #endif
 #endif
 
 #endif
